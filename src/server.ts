3c5c5f68b76b89e8376cd1a8475d85582cc464c4
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-06-30 02:16:09 -0300

    initil commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import http from'http';
+import express  from 'express';
+import  socketio from 'socket.io';
+import cors from 'cors';
+import morgan from 'morgan';
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+app.use(morgan('tiny'));
+
+app.use(cors());
+
+io.on("connect", (socket) => {
+  console.log("New client connected");
+  console.log(socket.id);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+
+
+server.listen(4000,()=>{
+  console.log('My server is up and running');
+})
