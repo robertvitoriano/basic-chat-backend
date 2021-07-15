f1fa09f16a09f64f95b40f7c3a1858126bd51e2e
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-07-15 06:18:22 -0300

    refactor: turn user utilities into class

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -1,56 +1,59 @@
-interface IUser{
-  username:string
-  id:string
-  room:string
+interface IUser {
+  username: string
+  id: string
+  room: string
 }
 
-const users: IUser[] = [{id:'1',username:'robert',room:'12'}]
+class Users {
 
-//add user
+  private users: IUser[] = []
 
-const addUser = ({username, id, room}:IUser)=>{
+  public addUser = ({ username, id, room }: IUser) => {
 
-  username  = username.trim().toLowerCase()
+    username = username.trim().toLowerCase()
 
-  id  = id.trim().toLowerCase()
+    id = id.trim().toLowerCase()
 
-  room  = room.trim().toLowerCase()
+    room = room.trim().toLowerCase()
 
-  if( !username || !room) throw new Error('Username and room are required !')
+    if (!username || !room) throw new Error('Username and room are required !')
 
-  const existingUser = users.find((user)=>user.room === room && user.username === username )
+    const existingUser = this.users.find((user) => user.room === room && user.username === username)
 
-  if(existingUser) throw new Error("username already in use !");
+    if (existingUser) throw new Error("username already in use !");
 
-  const user = {username, id, room}
+    const user = { username, id, room }
 
-  users.push(user)
+    this.users.push(user)
 
-  return user
-  
+    return user
+
+  }
+
+
+  public removeUser = (id) => {
+
+    const userToRemoveIndex = this.users.findIndex(user => user.id === id)
+
+    this.users.splice(userToRemoveIndex, 1)
+
+  }
+
+  public getUser = (id) => {
+
+    const user = this.users.find((user) => user.id == id)
+
+    return user
+
+  }
+
+  public getUsersInRoom = ({ room }) => {
+
+    const usersInRoom = this.users.filter((user) => user.room === room)
+
+    return usersInRoom
+  }
+
 }
 
-// remove user
-const removeUser = (id)=>{
-
-  const userToRemoveIndex = users.findIndex(user=> user.id ===id)
-
-  users.splice(userToRemoveIndex, 1)
-
-}
-// get user
-const getUser = (id)=>{
-
-  const user  = users.find((user)=>user.id==id)
-
-  return user
-
-}
-
-// get users in room
-const getUsersInRoom = ({room})=>{
-
-  const usersInRoom = users.filter((user)=>user.room===room)
-
-  return usersInRoom
-}
+export default new Users()
